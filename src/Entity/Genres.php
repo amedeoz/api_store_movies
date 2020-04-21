160014a362c1282c6d314c76f1af11c906765f8b
laurohen <laurohen@hotmail.com>
2020-04-21 17:00:30 -0300

    Create Genres.php

---
+++ src/Entity/Genres.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\GenresRepository")
+ */
+class Genres
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=30, nullable=true)
+     */
+    private $type;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Movies", inversedBy="genres_id", cascade={"persist", "remove"})
+     */
+    private $movies;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(?string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getMovies(): ?Movies
+    {
+        return $this->movies;
+    }
+
+    public function setMovies(?Movies $movies): self
+    {
+        $this->movies = $movies;
+
+        return $this;
+    }
+}
