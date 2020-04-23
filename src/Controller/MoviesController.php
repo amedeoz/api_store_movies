cdb61596d57516abd7238c50e535ecd8c75693ec
laurohen <laurohen@hotmail.com>
2020-04-23 05:59:06 -0300

    Update MoviesController.php

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -5,6 +5,9 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 Use Symfony\Component\HttpFoundation\Request;
+use Nelmio\ApiDocBundle\Annotation\Model;
+use Nelmio\ApiDocBundle\Annotation\Security;
+use Swagger\Annotations as SWG;
 use App\Entity\Movies;
 
 /**
@@ -15,28 +18,120 @@ class MoviesController extends AbstractController
     
     /**
      * @Route("/", name="index", methods={"GET"})
+     * @SWG\Get(
+	 * 		path="/movies/",
+	 * 		tags={"movies"},
+	 * 		operationId="index",
+	 * 		summary="List movies all.",
+	 * 		@SWG\Response(
+	 * 			response=200,
+	 * 			description="success",
+	 * 		),
+	 * 		@SWG\Response(
+	 * 			response=400,
+	 * 			description="Bad request",
+	 * 		),
+	 * 	)
      */
     public function index()
     {
         $movies = $this->getDoctrine()->getRepository(Movies::class)->findAll();
-        return $this->json([
-            'data' => $movies
+        return $this->json($movies, 200, [], [
+            'groups' => ['movies']
         ]);
     }
-
+    
     /**
      * @Route("/{moviesId}", name="show", methods={"GET"})
+     * @SWG\Get(
+	 * 		path="/movies/{moviesId}",
+	 * 		tags={"movies"},
+	 * 		operationId="show",
+	 * 		summary="List movie by id.",
+	 * 		@SWG\Response(
+	 * 			response=200,
+	 * 			description="success",
+	 * 		),
+	 * 		@SWG\Response(
+	 * 			response=400,
+	 * 			description="Bad request",
+	 * 		),
+	 * 	)
      */
     public function show($moviesId)
     {
         $movies = $this->getDoctrine()->getRepository(Movies::class)->find($moviesId);
-        return $this->json([
-            'data' => $movies
+        return $this->json($movies, 200, [], [
+            'groups' => ['movies']
         ]);
     }
 
     /**
      * @Route("/", name="create", methods={"POST"})
+     * @SWG\Post(
+	 * 		path="/movies/",
+	 * 		tags={"movies"},
+	 * 		operationId="create",
+	 * 		summary="Add new movies.",
+     *      consumes={"application/x-www-form-urlencoded"},
+     *      produces={"application/json"},
+     *      @SWG\Parameter(
+	 * 			name="genre",
+	 * 			in="formData",
+     *          type="string",
+	 * 			@SWG\Property(property="genre", type="string", example="action"),
+     *		),
+     *      @SWG\Parameter(
+	 * 			name="title",
+	 * 			in="formData",
+     *          type="string",
+	 * 			@SWG\Property(property="title", type="string", example="Nem Name movie"),
+     *		),
+     * 		@SWG\Parameter(
+	 * 			name="poster",
+	 * 			in="formData",
+     *          type="string",
+	 * 			@SWG\Property(property="poster", type="string", example="Nem URL poster"),
+     *		),
+     * 		@SWG\Parameter(
+	 * 			name="release_date",
+	 * 			in="formData",
+     *          type="string",
+	 * 			@SWG\Property(property="release_date", type="string", example="YYYY-mm-dd"),
+     *		),
+     * 		@SWG\Parameter(
+	 * 			name="duration",
+	 * 			in="formData",
+     *          type="string",
+	 * 			@SWG\Property(property="duration", type="string", example="120 minutes"),
+     *		),
+     * 		@SWG\Parameter(
+	 * 			name="description",
+	 * 			in="formData",
+     *          type="string",
+	 * 			@SWG\Property(property="description", type="string", example="Description movie"),
+     *		),
+     * 		@SWG\Parameter(
+	 * 			name="url_trailer",
+	 * 			in="formData",
+     *          type="string",
+	 * 			@SWG\Property(property="url_trailer", type="string", example="URL trailer"),
+     *		),
+     * 		@SWG\Parameter(
+	 * 			name="week_number",
+	 * 			in="formData",
+     *          type="integer",
+	 * 			@SWG\Property(property="week_number", type="string", example="10"),
+	 *		),	
+	 * 		@SWG\Response(
+	 * 			response=200,
+	 * 			description="success",
+	 * 		),
+	 * 		@SWG\Response(
+	 * 			response=400,
+	 * 			description="Bad request",
+	 * 		),
+	 * 	)
      */
     public function create(Request $request)
     {
@@ -65,6 +160,148 @@ class MoviesController extends AbstractController
 
     /**
      * @Route("/{moviesId}", name="update", methods={"PUT" , "PATCH"})
+     * @SWG\Put(
+	 * 		path="/movies/{moviesId}",
+	 * 		tags={"movies"},
+	 * 		operationId="updatePut",
+	 * 		summary="Update movies.",
+     *      consumes={"application/x-www-form-urlencoded"},
+     *      produces={"application/json"},
+     *      @SWG\Parameter(
+	 * 			name="id",
+	 * 			in="path",
+     *          required=true,
+     *          type="integer",
+     *          description="Put id movies",
+     *		),
+     *      @SWG\Parameter(
+	 * 			name="genre",
+	 * 			in="formData",
+     *          type="string",
+	 * 			@SWG\Property(property="genre", type="string", example="action"),
+     *		),
+     *      @SWG\Parameter(
+	 * 			name="title",
+	 * 			in="formData",
+     *          type="string",
+	 * 			@SWG\Property(property="title", type="string", example="Nem Name movie"),
+     *		),
+     * 		@SWG\Parameter(
+	 * 			name="poster",
+	 * 			in="formData",
+     *          type="string",
+	 * 			@SWG\Property(property="poster", type="string", example="Nem URL poster"),
+     *		),
+     * 		@SWG\Parameter(
+	 * 			name="release_date",
+	 * 			in="formData",
+     *          type="string",
+	 * 			@SWG\Property(property="release_date", type="string", example="YYYY-mm-dd"),
+     *		),
+     * 		@SWG\Parameter(
+	 * 			name="duration",
+	 * 			in="formData",
+     *          type="string",
+	 * 			@SWG\Property(property="duration", type="string", example="120 minutes"),
+     *		),
+     * 		@SWG\Parameter(
+	 * 			name="description",
+	 * 			in="formData",
+     *          type="string",
+	 * 			@SWG\Property(property="description", type="string", example="Description movie"),
+     *		),
+     * 		@SWG\Parameter(
+	 * 			name="url_trailer",
+	 * 			in="formData",
+     *          type="string",
+	 * 			@SWG\Property(property="url_trailer", type="string", example="URL trailer"),
+     *		),
+     * 		@SWG\Parameter(
+	 * 			name="week_number",
+	 * 			in="formData",
+     *          type="integer",
+	 * 			@SWG\Property(property="week_number", type="string", example="10"),
+	 *		),	
+	 * 		@SWG\Response(
+	 * 			response=200,
+	 * 			description="success",
+	 * 		),
+	 * 		@SWG\Response(
+	 * 			response=400,
+	 * 			description="Bad request",
+	 * 		),
+	 * 	)
+     *  @SWG\Patch(
+	 * 		path="/movies/{moviesId}",
+	 * 		tags={"movies"},
+	 * 		operationId="updatePatch",
+	 * 		summary="Update movies.",
+     *      consumes={"application/x-www-form-urlencoded"},
+     *      produces={"application/json"},
+     *      @SWG\Parameter(
+	 * 			name="id",
+	 * 			in="path",
+     *          required=true,
+     *          type="integer",
+     *          description="Put id movies",
+     *		),
+     *      @SWG\Parameter(
+	 * 			name="genre",
+	 * 			in="formData",
+     *          type="string",
+	 * 			@SWG\Property(property="genre", type="string", example="action"),
+     *		),
+     *      @SWG\Parameter(
+	 * 			name="title",
+	 * 			in="formData",
+     *          type="string",
+	 * 			@SWG\Property(property="title", type="string", example="Nem Name movie"),
+     *		),
+     * 		@SWG\Parameter(
+	 * 			name="poster",
+	 * 			in="formData",
+     *          type="string",
+	 * 			@SWG\Property(property="poster", type="string", example="Nem URL poster"),
+     *		),
+     * 		@SWG\Parameter(
+	 * 			name="release_date",
+	 * 			in="formData",
+     *          type="string",
+	 * 			@SWG\Property(property="release_date", type="string", example="YYYY-mm-dd"),
+     *		),
+     * 		@SWG\Parameter(
+	 * 			name="duration",
+	 * 			in="formData",
+     *          type="string",
+	 * 			@SWG\Property(property="duration", type="string", example="120 minutes"),
+     *		),
+     * 		@SWG\Parameter(
+	 * 			name="description",
+	 * 			in="formData",
+     *          type="string",
+	 * 			@SWG\Property(property="description", type="string", example="Description movie"),
+     *		),
+     * 		@SWG\Parameter(
+	 * 			name="url_trailer",
+	 * 			in="formData",
+     *          type="string",
+	 * 			@SWG\Property(property="url_trailer", type="string", example="URL trailer"),
+     *		),
+     * 		@SWG\Parameter(
+	 * 			name="week_number",
+	 * 			in="formData",
+     *          type="integer",
+	 * 			@SWG\Property(property="week_number", type="string", example="10"),
+	 *		),	
+	 * 		@SWG\Response(
+	 * 			response=200,
+	 * 			description="success",
+	 * 		),
+	 * 		@SWG\Response(
+	 * 			response=400,
+	 * 			description="Bad request",
+	 * 		),
+	 * 	)
      */
     public function update($moviesId, Request $request)
     {
@@ -108,6 +345,20 @@ class MoviesController extends AbstractController
 
     /**
      * @Route("/{moviesId}", name="delete", methods={"DELETE"})
+     * @SWG\Delete(
+	 * 		path="/movies/{moviesId}",
+	 * 		tags={"movies"},
+	 * 		operationId="delete",
+	 * 		summary="Remove movie by id",
+	 * 		@SWG\Response(
+	 * 			response=200,
+	 * 			description="success",
+	 * 		),
+	 * 		@SWG\Response(
+	 * 			response=400,
+	 * 			description="Bad request",
+	 * 		),
+	 * 	)
      */
     public function delete($moviesId)
     {
@@ -124,6 +375,20 @@ class MoviesController extends AbstractController
 
     /**
      * @Route("/search/{value}", name="search", methods={"GET"})
+     * @SWG\Get(
+	 * 		path="/movies/search/{value}",
+	 * 		tags={"movies"},
+	 * 		operationId="search",
+	 * 		summary="Search movies for title or genre.",
+	 * 		@SWG\Response(
+	 * 			response=200,
+	 * 			description="success",
+	 * 		),
+	 * 		@SWG\Response(
+	 * 			response=400,
+	 * 			description="Bad request",
+	 * 		),
+	 * 	)
      */
     public function search($value)
     {
@@ -131,7 +396,9 @@ class MoviesController extends AbstractController
             $movies = $this->getDoctrine()->getRepository(Movies::class)->findByValue($value);
             return $this->json([
                 'message' => 'List film by genre/title',
-                'data' => $movies
+                'data' => $movies, 200, [], [
+                    'groups' => ['movies']
+                ]
             ]);
         }else{
             return $this->json([
@@ -143,14 +410,27 @@ class MoviesController extends AbstractController
 
     /**
      * @Route("/search_genre/{genre}", name="search_genre", methods={"GET"})
+     * @SWG\Get(
+	 * 		path="/movies/search_genre/{genre}",
+	 * 		tags={"movies"},
+	 * 		operationId="search_genre",
+	 * 		summary="Search movies for genre.",
+	 * 		@SWG\Response(
+	 * 			response=200,
+	 * 			description="success",
+	 * 		),
+	 * 		@SWG\Response(
+	 * 			response=400,
+	 * 			description="Bad request",
+	 * 		),
+	 * 	)
      */
     public function search_genre($genre)
     {
         if(strlen($genre) > 3){
             $movies = $this->getDoctrine()->getRepository(Movies::class)->findByGenre($genre);
-            return $this->json([
-                'message' => 'List film by genre',
-                'data' => $movies
+            return $this->json($movies, 200, [], [
+                'groups' => ['movies']
             ]);
         }else{
             return $this->json([
@@ -162,14 +442,27 @@ class MoviesController extends AbstractController
 
     /**
      * @Route("/search_title/{title}", name="search_title", methods={"GET"})
+     * @SWG\Get(
+	 * 		path="/movies/search_title/{title}",
+	 * 		tags={"movies"},
+	 * 		operationId="search_title",
+	 * 		summary="Search movies for title.",
+	 * 		@SWG\Response(
+	 * 			response=200,
+	 * 			description="success",
+	 * 		),
+	 * 		@SWG\Response(
+	 * 			response=400,
+	 * 			description="Bad request",
+	 * 		),
+	 * 	)
      */
     public function search_title($title)
     {
         if(strlen($title) > 3){
             $movies = $this->getDoctrine()->getRepository(Movies::class)->findByTitle($title);
-            return $this->json([
-                'message' => 'List film by title',
-                'data' => $movies
+            return $this->json($movies, 200, [], [
+                'groups' => ['movies']
             ]);
         }else{
             return $this->json([
