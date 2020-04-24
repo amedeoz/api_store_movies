c1d552652d70e8b312c258d7f53df495c0fb120d
laurohen <laurohen@hotmail.com>
2020-04-24 14:25:42 -0300

    Update WeekController.php

--- src/Controller/WeekController.php
+++ src/Controller/WeekController.php
@@ -4,10 +4,13 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+Use Symfony\Component\HttpFoundation\Request;
 use Nelmio\ApiDocBundle\Annotation\Model;
 use Nelmio\ApiDocBundle\Annotation\Security;
 use Swagger\Annotations as SWG;
+use App\Entity\Weeks;
 use App\Entity\Movies;
+
 
 /**
 * @Route("/weeks", name="week_")
@@ -15,9 +18,9 @@ use App\Entity\Movies;
 class WeekController extends AbstractController
 {
     /**
-     * @Route("/{weekNum}", name="show", methods={"GET"})
+     * @Route("/search_week/{weekNum}", name="search_week", methods={"GET"})
      * @SWG\Get(
-	 * 		path="/weeks/{weekNum}",
+	 * 		path="/weeks/search_week/{weekNum}",
 	 * 		tags={"weeks"},
 	 * 		operationId="weeks",
 	 * 		summary="List movies by number week.",
@@ -31,13 +34,19 @@ class WeekController extends AbstractController
 	 * 		),
 	 * 	)
      */
-    public function show($weekNum)
+    public function search_week($weekNum)
     {
-        $movies = $this->getDoctrine()->getRepository(Movies::class)->findBy(['week_number' => $weekNum]);
-        return $this->json([
-            'message' => 'List film week',
-            'data' => $movies
-        ]);
-
+		if(strlen($weekNum) == 2){
+			$movies = $this->getDoctrine()->getRepository(Weeks::class)->findByWeeks($weekNum);
+			return $this->json($movies, 200, [], [
+                'groups' => ['movies']
+            ]);
+		}else{
+            return $this->json([
+                'message' => 'Min 2 length. Value 01 - 48',
+                'data' => strlen($weekNum)
+            ]);
+		}
+		
     }
 }
