1ccb98a39edb2afc84cb0679e434f096bf71eaa0
laurohen <laurohen@hotmail.com>
2020-04-24 14:25:17 -0300

    Update store_movies_week.sql

--- bd/store_movies_week.sql
+++ bd/store_movies_week.sql
@@ -1,5 +1,5 @@
-﻿# Host:  (Version 5.5.5-10.2.11-MariaDB-log)
-# Date: 2020-04-21 17:25:44
+﻿# Host: database-dev.cuu6et9mqpeu.us-east-1.rds.amazonaws.com  (Version 5.5.5-10.2.11-MariaDB-log)
+# Date: 2020-04-24 14:23:45
 # Generator: MySQL-Front 6.1  (Build 1.26)
 
 
@@ -18,7 +18,7 @@ CREATE TABLE `migration_versions` (
 # Data for table "migration_versions"
 #
 
-INSERT INTO `migration_versions` VALUES ('20200421195424','2020-04-21 19:54:44');
+INSERT INTO `migration_versions` VALUES ('20200424172117','2020-04-24 17:22:40');
 
 #
 # Structure for table "movies"
@@ -27,16 +27,14 @@ INSERT INTO `migration_versions` VALUES ('20200421195424','2020-04-21 19:54:44')
 DROP TABLE IF EXISTS `movies`;
 CREATE TABLE `movies` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `genre` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `title` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `poster` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `relase_date` date NOT NULL,
-  `duration` varchar(15) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `description` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `url_trailer` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `week_number` varchar(2) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created_at` datetime NOT NULL,
-  `updated_at` datetime NOT NULL,
+  `title` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `poster` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `release_date` date DEFAULT NULL,
+  `duration` varchar(15) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `description` longtext COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `url_trailer` longtext COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -71,8 +69,8 @@ CREATE TABLE `genres` (
 DROP TABLE IF EXISTS `weeks`;
 CREATE TABLE `weeks` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `movies_id` int(11) NOT NULL,
-  `number_week` int(11) NOT NULL,
+  `movies_id` int(11) DEFAULT NULL,
+  `number` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `IDX_803157D253F590A4` (`movies_id`),
   CONSTRAINT `FK_803157D253F590A4` FOREIGN KEY (`movies_id`) REFERENCES `movies` (`id`)
